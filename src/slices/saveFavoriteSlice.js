d64282ca7bf2108a54f14eab0e0db9de954e2733
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-12-09 11:51:10 -0800

    Update favorites logic to store locally

--- src/slices/saveFavoriteSlice.js
+++ src/slices/saveFavoriteSlice.js
@@ -1,18 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const saveFavoritesToLocalStorage = (favorites) => {
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+};
+
+const getFavoritesFromLocalStorage = () => {
+  const favorites = localStorage.getItem("favorites");
+  if (favorites) {
+    return JSON.parse(favorites);
+  }
+  return [];
+};
+
 export const saveFavoriteSlice = createSlice({
   name: "saveFavorite",
   initialState: {
-    favorites: [],
+    favorites: getFavoritesFromLocalStorage(),
   },
   reducers: {
     saveFavorite: (state, action) => {
       state.favorites.push(action.payload);
+      saveFavoritesToLocalStorage(state.favorites);
     },
     removeFavorite: (state, action) => {
       state.favorites = state.favorites.filter(
         (favorite) => favorite.id !== action.payload.id
       );
+      saveFavoritesToLocalStorage(state.favorites);
     },
   },
 });
