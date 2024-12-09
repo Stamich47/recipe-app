bb11b7d37c71a6842faf809d08ae42d1bcc51c54
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-12-09 11:33:52 -0800

    Add backlink to Spoonacular API in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ export default function Footer() {
   return (
     <div className="bg-gray-500 text-white text-center p-4 my-6 rounded">
       <p>&copy; 2024 BiteSized</p>
+      <p>
+        Data provided by <a href="https://spoonacular.com">Spoonacular</a>
+      </p>
     </div>
   );
 }
