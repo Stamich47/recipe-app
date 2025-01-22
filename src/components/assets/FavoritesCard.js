13bfb63522709dfb376848b8a33fa9c5209cf5ab
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2025-01-22 15:59:58 -0800

    Add remove-favorite feature from Favorites Page

--- src/components/assets/FavoritesCard.js
+++ src/components/assets/FavoritesCard.js
@@ -1,21 +1,45 @@
 import { FaHeart } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { removeFavorite } from "../../slices/saveFavoriteSlice";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { LuVegan } from "react-icons/lu";
 
 export default function FavoritesCard({ favorite }) {
+  const [isVegan, setIsVegan] = useState(false);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setIsVegan(favorite.vegan);
+  }, [favorite.vegan]);
+
+  console.log(favorite);
+
+  const handleRemoveLike = (e) => {
+    e.stopPropagation();
+    dispatch(removeFavorite(favorite));
+  };
+
   return (
     <div
       key={favorite.id}
-      className="flex flex-col bg-white shadow-lg rounded-lg"
+      className="flex flex-col bg-white shadow-lg rounded-md results-card"
     >
       <div className="relative rounded-lg overflow-hidden">
-        <img
-          src={favorite.image}
-          alt={favorite.title}
-          className="w-full h-52 object-cover"
-        />
+        <Link key={favorite.id} to={`/recipe/${favorite.id}`}>
+          <img
+            src={favorite.image}
+            alt={favorite.title}
+            className="w-full h-52 object-cover"
+          />
+        </Link>
         <div className="absolute top-0 right-0 m-4">
-          <button>
-            <FaHeart color={"#f27e8a"} size={20} />
+          <button className="heart-button" onClick={handleRemoveLike}>
+            <FaHeart color={"#f27e8a"} className="heart-icon" />
           </button>
+        </div>
+        <div className="absolute top-0 left-0 m-2">
+          {isVegan && <LuVegan color={"#4CAF50"} className="vegan-icon" />}
         </div>
         <div className="absolute bottom-0 bg-gradient-to-t from-gray-800 to-transparent w-full p-2">
           <div className="text-s font-bold text-white bg-opacity-40 p-1 pt-6 rounded-lg ">
--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,6 +1,4 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-
 import FavoritesCard from "../components/assets/FavoritesCard";
 
 export default function FavoritesPage() {
@@ -11,15 +9,14 @@ export default function FavoritesPage() {
       <h1 className="text-2xl font-bold text-center mb-4">
         My Favorite Recipes
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+      <div className="flex flex-wrap justify-center gap-4">
         {favorites.length === 0 && (
-          <div className="flex justify-center ">No favorites yet!</div>
+          <div className="flex justify-center">No favorites yet!</div>
         )}
         {favorites.length > 0 &&
           favorites.map((favorite) => (
-            <Link key={favorite.id} to={`/recipe/${favorite.id}`}>
-              <FavoritesCard key={favorite.id} favorite={favorite} />
-            </Link>
+            <FavoritesCard key={favorite.id} favorite={favorite} />
           ))}
       </div>
     </div>
